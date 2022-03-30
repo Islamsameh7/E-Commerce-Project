4b746630ba558ca69906353dcdf8b2fa357e881e
Islamsameh7 <islamsameh33@gmail.com>
2022-03-30 17:55:26 +0200

    Project

--- ECommerce/pages/views.py
+++ ECommerce/pages/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    x = {'name':'ali', 'age':25}
-    return render(request, 'templates/pages/index.html', x)
+    return render(request, 'templates/pages/index.html')
