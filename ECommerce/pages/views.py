9aec9e57284468b2fc6baf3d70181528dbe927a1
AhmedMagdy2001 <ahmedmagdy3abdelfattah@gmail.com>
2022-05-12 00:16:32 +0200

    added shop.html and added the products model ,
    now the admin can add products manually

--- ECommerce/pages/views.py
+++ ECommerce/pages/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from pages.models import Product
+
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'templates/pages/index.html')
+def home(request):
+    return render(request, 'pages/home.html')
+
+def shop(request):
+    products = Product.objects.all()
+    context ={'products':products}
+    
+    return render(request, 'pages/shop.html' , context)
+    
