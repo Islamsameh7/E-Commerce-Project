9f1f3d643b97b2ef32781fad77abf2fd530afa5e
AhmedMagdy2001 <ahmedmagdy3abdelfattah@gmail.com>
2022-05-27 01:14:20 +0200

    update

--- ECommerce/static/js/register.js
+++ ECommerce/static/js/register.js
@@ -1,37 +1,32 @@
-var name = document.getElementById('name')
-var password = document.getElementById('password')
-var form = document.getElementById('form')
-var errorElement = document.getElementById('error')
+var name = document.getElementById("name");
+var password = document.getElementById("password");
+var form = document.getElementById("form");
+var errorElement = document.getElementById("error");
+var ConfirmPassword = document.getElementById("confirm-password");
 
-form.addEventListener('submit', (e) => {
-  let messages = []
-
+form.addEventListener("submit", (e) => {
+  let messages = [];
 
   if (password.value.length <= 6) {
-    messages.push('Password must be longer than 6 characters')
+    messages.push("Password must be longer than 6 characters");
   }
 
   if (password.value.length >= 20) {
-    messages.push('Password must be less than 20 characters')
+    messages.push("Password must be less than 20 characters");
   }
 
-  if (password.value === 'password') {
-    messages.push('Password cannot be password')
+  if (password.value === "password") {
+    messages.push("Password cannot be password");
   }
-
+  if (password.value.length != 0) {
+    if (password.value != ConfirmPassword.value) {
+        messages.push("Passwords must match");
+    }
+  }
   if (messages.length > 0) {
-    e.preventDefault()
-    errorElement.innerText = messages.join(', ')
+    e.preventDefault();
+    errorElement.innerText = messages.join(", ");
   }
-  var ConfirmPassword = document.getElementById("Confirm-password").value;
-  console.log(password.value,ConfirmPassword);
-
-  let message = document.getElementById("message");
-
-  if(password.value.length !=0){
-      if(password.value != ConfirmPassword){
-          message.textContent ="password doesn't match";
-      }
-  }
-})
-
+  
+ 
+});
